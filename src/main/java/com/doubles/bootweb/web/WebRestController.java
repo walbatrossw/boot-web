2669ba27cc5d3bc6ad72d4d0f67efe63e92e0f24
walbatrossw <walbatrossw@gmail.com>
2019-03-11 12:13:28 +0900

    Feat : 스프링 부트로 웹서비스 출시하기 프로젝트 생성 및 깃허브 연동
    - Spring-Boot, Gradle
    - Dependencies : DevTools, Lombok, Web, JPA, H2, Actuators

---
+++ src/main/java/com/doubles/bootweb/web/WebRestController.java
@@ -0,0 +1,14 @@
+package com.doubles.bootweb.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world";
+    }
+
+}
